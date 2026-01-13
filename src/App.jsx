6e5c6773aa136fb5bf699a308f08deff00ee6e37
Alexandros <alexandrospavlidis@monzo.com>
2026-01-13 20:19:22 +0000

    fix scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
+import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/index";
 import NotFound from "./pages/notFound";
 import Contact from "./pages/contact";
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/contact" element={<Contact />} />
